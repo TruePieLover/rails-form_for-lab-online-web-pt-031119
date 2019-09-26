ff70b2b4d124b18e3bf4c577dc4b8cd98d35f140
Brandon Teich <brandonteich123@gmail.com>
2019-09-26 18:59:18 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,31 +1,30 @@
-class StudentsController < ApplicationController 
-  
-  def new
-   @student= Student.new
-  end
-  
-  def create
-    var =Student.new(student_params)
-    var.save
-    redirect_to student_path(var)
-  end 
-  
-  def show
-    @student = Student.find(params[:id])
-  end
-  
-  def edit
-    @student = Student.find(params[:id])
-  end
-  
-  def update
-    var= Student.find(params[:id])
-    var.update(student_params)
-    redirect_to  student_path(var)
-  end
+	class StudentsController < ApplicationController
+    def new
+        @student = Student.new
+    end
 
-  def student_params
-    params.require(:student).permit(:first_name, :last_name)
-  end
+    def show
+        @student = Student.find(params[:id])
+    end
 
-end
+    def create
+        @student = Student.new(post_params)
+        @student.save
+        redirect_to student_path(@student)
+    end
+
+    def edit
+        @student = Student.find(params[:id])
+    end
+
+    def update
+        @student = Student.find(params[:id])
+        @student.update(post_params)
+        redirect_to student_path(@student)
+    end
+
+    def post_params
+        params.require(:student).permit(:first_name, :last_name)
+    end
+
+end 
