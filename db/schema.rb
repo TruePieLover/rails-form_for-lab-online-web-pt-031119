96395886bde3a2e2d35c924f71b576206bf907d8
Brandon Teich <brandonteich123@gmail.com>
2019-09-26 19:08:44 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+  
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,11 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190322195635) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "school_classes", force: :cascade do |t|
-    t.string  "title"
-    t.integer "room_number"
+    t.string   "title"
+    t.integer  "room_number"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "students", force: :cascade do |t|
