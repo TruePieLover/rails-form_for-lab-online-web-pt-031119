d8277c59dfb318165629385b9c390365f7f969fb
Brandon Teich <brandonteich123@gmail.com>
2019-09-26 18:58:45 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,31 +1,31 @@
-class SchoolClassesController < ApplicationController 
-  
-  def new
-    @school_class = SchoolClass.new
-  end
-  
-  def create
-    var =SchoolClass.new(school_class_params)
-    var.save
-    redirect_to school_class_path(var)
-  end 
-  
-  def show
-    @school_class = SchoolClass.find(params[:id])
-  end   
-  
-  def edit
-    @school_class = SchoolClass.find(params[:id])
-  end
-  
-  def update
-    var=SchoolClass.find(params[:id])
-    var.update(school_class_params)
-    redirect_to  school_class_path(var)
-  end
-  
-  def school_class_params
-    params.require(:school_class).permit(:title, :room_number)
-  end
-  
-end  
+class SchoolClassesController < ApplicationController
+
+    def new
+        @school = SchoolClass.new
+    end
+
+    def show
+        @school = SchoolClass.find(params[:id])
+    end
+
+    def create
+        @school = SchoolClass.new(post_params)
+        @school.save
+        redirect_to school_class_path(@school)
+    end
+
+    def edit
+        @school = SchoolClass.find(params[:id])
+    end
+    
+    def update
+        @school = SchoolClass.find(params[:id])
+        @school.update(post_params)
+        redirect_to school_class_path(@school)
+    end
+
+    def post_params
+        params.require(:school_class).permit(:title, :room_number)
+    end
+
+end
