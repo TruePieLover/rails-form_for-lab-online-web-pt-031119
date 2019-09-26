051b14fdf94f6b4f59ffee6bcb23eea7e7f78328
Brandon Teich <brandonteich123@gmail.com>
2019-09-26 19:10:22 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,31 +1,36 @@
 class SchoolClassesController < ApplicationController
-  
-    def new
-      @school = SchoolClass.new
-    end
 
-    def show
-      @school = SchoolClass.find(params[:id])
-    end
+  def index
+    @school_classes = SchoolClass.all
+  end
 
-    def create
-      @school = SchoolClass.new(post_params)
-      @school.save
-      redirect_to school_class_path(@school)
-    end
+  def new
+    @school_class = SchoolClass.new
+  end
 
-    def edit
-      @school = SchoolClass.find(params[:id])
-    end
-    
-    def update
-      @school = SchoolClass.find(params[:id])
-      @school.update(post_params)
-      redirect_to school_class_path(@school)
-    end
+  def create
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
 
-    def post_params
-      params.require(:school_class).permit(:title, :room_number)
-    end
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
 
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
+  end
 end
